f2f3c7ccbc49fc18d92d91c36af0636cbb6632e5
kristinadurant <kristinadurant.p@gmail.com>
2021-02-17 09:24:43 -0500

    connected firestore for emails

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { db } from '../firebase';
 
 const Form = () => {
+    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({});
 
-const handleSubmit = () => {
-    
-}
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };    
+
+    const handleSubmit = async (e) => {
+        const form = e.target;
+        e.preventDefault();
+        setLoading(true);
+        db.collection("emails").add(formData)
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            setFormData({});
+            form.reset();
+        })
+        .catch((error) => {
+            console.log(error);      
+        });
+        setLoading(false);
+    }
 
     return (
         <form onSubmit={handleSubmit}>
+
             <div>
                 <label htmlFor='name' className='required'>
                     Name:
                 </label>
-                <input type='text' id='name' name='name' required />
+                <input onChange={handleChange} type='text' id='name' name='name' required />
             </div>
             <div>
                 <label htmlFor='email' className='required'>
                     Email:
                 </label>
-                <input type='email' id='email' name='email' required />
+                <input onChange={handleChange} type='email' id='email' name='email' required />
             </div>
             <div>
-                <label htmlFor='message' className='required'>Message:</label>
-                <textarea id='message' name='message' rows="4" required/>
+                <label htmlFor='message' className='required'>
+                    Message:
+                </label>
+                <textarea onChange={handleChange} id='message' name='message' rows="4" required/>
             </div>
-            <button type='submit'>Send Message</button>
+
+            <button type='submit' disabled={loading}>Send Message</button>
+
         </form>
     )
 }
