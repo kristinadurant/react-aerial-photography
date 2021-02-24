866bd69c6338b96daa56371f12ddbc60e44b0428
kristinadurant <kristinadurant.p@gmail.com>
2021-02-24 13:15:23 -0500

    disable when selected

--- src/components/gallery/Image.jsx
+++ src/components/gallery/Image.jsx
@@ -1,17 +1,26 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { AppContext } from '../../context/AppContext';
 
 const Image = ({ img }) => {
-    const { addFavorite } = useContext(AppContext);
+    const { favorites, addFavorite } = useContext(AppContext);
     const [ selected, setSelected ] = useState(false);
+
+    function handleClick(img) {
+        addFavorite(img);
+        setSelected(true);
+    }
+    useEffect(()=> {
+        if(favorites.includes(img)) setSelected(true);
+        console.log(selected);
+    }, []);
 
     return (
         <div key={img.id} className='container'>
             <img src={require(`../../assets/images/${img.url}`).default} alt={img.description} height='auto' width='100%' />       
             <div className='overlay'>
-                <button onClick={() => addFavorite(img)} value={img}>
+                <button onClick={() => handleClick(img)} disabled={selected} >
                     <span className='hide'>Favorites</span>
-                    {selected || img.favorite
+                    {selected
                     ? <i className="fas fa-heart"></i>
                     : <i className="far fa-heart"></i>
                     }
