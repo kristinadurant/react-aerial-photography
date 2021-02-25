1d343a924b2670fbd6e2059081e48724806ef59a
kristinadurant <kristinadurant.p@gmail.com>
2021-02-25 00:23:24 -0500

    added steps for downloading images to the section

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -5,18 +5,22 @@ import ImagesCarousel from './ImagesCarousel';
 const Carousel = () => {
     return (
         <section className='carousel full-screen'>
-            <ImagesCarousel />
-            <div className='overlay'>
+            <div className='title'>
                 <div className='inner'>
-                    <p>
-                        Breathtaking Views
-                    </p>
+                    <h2 style={{ fontSize: '1em'}}>
+                        <p className='font2'>High Resolution</p>
+                        <p className='font1'>Photos & Videos</p>
+                    </h2>
+                    <ol> 
+                        <li>- Bookmark Images</li>
+                        <li>- Send an Inquiry</li>
+                        <li>- Choose type of Payment</li>
+                        <li>- Receive High Resolution Files</li>
+                    </ol>
+                    <Link to='/gallery'>View Gallery</Link>
                 </div>
             </div>
-            <div className='link'>
-                <Link to='/gallery'>View Gallery</Link>
-                <i className="fas fa-long-arrow-alt-right"></i>
-            </div>
+            <ImagesCarousel />
         </section>
     )
 }
