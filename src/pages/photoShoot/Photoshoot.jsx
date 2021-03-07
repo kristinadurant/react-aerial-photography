bb9ab610b6897b054f87335995c78445ba412f8f
kristinadurant <kristinadurant.p@gmail.com>
2021-03-07 08:53:05 -0500

    added helmet to Photo Shoot

--- src/pages/photoShoot/Photoshoot.jsx
+++ src/pages/photoShoot/Photoshoot.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Banner, Steps } from '../../components';
 import { bannerImage, steps } from './Content';
 
 const Photoshoot = () => {
     return (
         <main>
+            <Helmet>
+                <title>Aerial Photo and Video Shoots</title>
+                <meta name='description' content='Schedule a Photo and Video Shoot in Miami with Drone. ' />
+                <meta name='keywords' content='Aerial Photo Video Shoot Drone Miami' />
+            </Helmet>
+
             <Banner title='Aerial Photo & Video Shoot' image={bannerImage}>
                 <p className='font2' style={{ fontWeight: 'bold'}}>Miami, FL</p>
                 <button className='button'>Schedule Now</button>
