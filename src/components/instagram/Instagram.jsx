68b9dccf31dac4c586e97259b07b6e86555b864e
kristinadurant <kristinadurant.p@gmail.com>
2021-03-16 18:12:39 -0400

    fetched ig name

--- src/components/instagram/Instagram.jsx
+++ src/components/instagram/Instagram.jsx
@@ -3,25 +3,39 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Instagram = () => {
-    const [apiData, setApiData ] = useState(null);
+    const [apiData, setApiData] = useState(null);
+    const [profile, setProfile] = useState({});
 
     useEffect(()=> {
         axios.get(`https://graph.instagram.com/me/media?fields=id,media_url,permalink&limit=3&access_token=${process.env.REACT_APP_IG_TOKEN}`)
-        .then(response => {
-            setApiData(response.data.data);
-        });
+        .then(response => setApiData(response.data.data));
+    }, []);
+
+    useEffect(()=> {
+        axios.get(`https://graph.instagram.com/me?fields=username&access_token=${process.env.REACT_APP_IG_TOKEN}`)
+        .then(response => setProfile(response.data.username));
     }, []);
 
     return apiData && (
         <div className='instagram-feed inner'>
-            {apiData.map(post => (
-                <div key={post.id} className='background-image' style={{backgroundImage: `url(${post.media_url})`}}>
-                    <a href={post.permalink} target='_blank' rel='noreferrer'>
-                        <span className='hide'>View Instagram Post</span>
-                        <i className='fab fa-instagram'></i>
+            <h2 className='two-lines'>
+                <span className='font2'>Latest on Instagram</span>
+                <span className='font1'>
+                    <a href={`https://www.instagram.com/${profile}`} target='_blank' rel='noreferrer'>
+                        @{profile}
                     </a>
-                </div>
-            ))}
+                </span>
+            </h2>
+            <div className='container'>
+                {apiData.map(post => (
+                    <div key={post.id} className='background-image' style={{backgroundImage: `url(${post.media_url})`}}>
+                        <a href={post.permalink} target='_blank' rel='noreferrer'>
+                            <span className='hide'>View Instagram Post</span>
+                            <i className='fab fa-instagram'></i>
+                        </a>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
