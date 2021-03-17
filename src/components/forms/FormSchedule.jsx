1b8fde27e97d1bed030763f9f64fc94ceb4be1f8
kristinadurant <kristinadurant.p@gmail.com>
2021-03-17 12:46:39 -0400

    redo of form schedule

--- src/components/forms/FormSchedule.jsx
+++ src/components/forms/FormSchedule.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { InputText, InputTextarea } from './components';
 
 const FormSchedule = () => {
     return (
         <>
+            <InputText label='Name' id='name' required />
+            <InputText label='Email' id='email' required type='email' />
             <div>
                 <label htmlFor='date'>
                     Date of Photo/Video Shoot <strong title="required" aria-label='required'>*</strong>
@@ -13,12 +16,7 @@ const FormSchedule = () => {
                     data-date-inline-picker="true"
                 />
             </div>
-            <div>
-                <label htmlFor='message'>
-                    Tell us a bit more about yourself or your project <strong title="required" aria-label='required'>*</strong>
-                </label>
-                <textarea id='message' name='message' rows="4" required/>
-            </div>
+            <InputTextarea label='Tell us a bit more about yourself or your project' id='message' rows='4' required/>
         </>
     )
 }
